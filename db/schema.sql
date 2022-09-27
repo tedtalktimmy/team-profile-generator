8327ba7eb7db9428feaf6a78645e58034c3382cc
tedtalktimmy <frtntn007@gmail.com>
2022-09-26 22:18:26 -0400

    created db.sql and schema.sql as well as connection.js

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
+
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary FLOAT,
+    department_id INT,
+);
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL,
+);
