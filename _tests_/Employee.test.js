87bdc720bedf879a747466cd2613bfa8f36759b5
tedtalktimmy <frtntn007@gmail.com>
2022-10-08 02:11:07 -0400

    created properties and methods for Employe, Manager, Engineer, Intern classes

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,5 +1,21 @@
 const Employee = require("../lib/Employee");
 
-describe("Employee", () => {
+test('creates new employee', () => {
+  const employee = new Employee('Jim Jones', 12345, 'jim@jones.com');
+
+  expect(employee.name).toBe('Jim Jones');
+  expect(employee.id).toEqual(expect.any(Number));
+  expect(employee.email).toBe(expect.any(String));
+
+});
+
+
+test('checks for Employee class', () => {
+  const employee = new Employee('Jim Jones', 12345, 'jim@jones.com');
+
+  expect(employee.getName()).toBe(employee.name);
+  expect(employee.getId()).toEqual(employee.id);
+  expect(employee.getEmail()).toBe(employee.email);
+  expect(employee.getRole()).toBe('Employee');
 
 });
